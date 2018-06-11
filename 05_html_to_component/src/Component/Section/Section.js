1c13cd7e9680458396694c3d4f15367b47756d80
Dao Nguyen <daonguyen.dev@gmail.com>
2018-06-11 15:41:58 +0700

    How to write function to interactive programming

--- 05_html_to_component/src/Component/Section/Section.js
+++ 05_html_to_component/src/Component/Section/Section.js
@@ -5,6 +5,18 @@ class Section extends Component {
         super(props);
     }
 
+    notificationNoArgs = () => {
+        alert('Notification no args');
+    }
+
+    notificationHaveArgsUsingArrowFunction = (notify) => {
+        alert(notify);
+    }
+
+    notificationHaveArgsUsingBind = (notify) => {
+        alert(notify);
+    }
+
     render() {
         return (
             <section>
@@ -19,6 +31,16 @@ class Section extends Component {
                     <div className="p-5">
                         <h2 className="display-4">{this.props.title}</h2>
                         <p>{this.props.description}</p>
+                        <div className="row">
+                            <div className="btn btn-group">
+                            <div className="btn btn-info" 
+                                    onClick={this.notificationNoArgs}>Edit</div>
+                            <div className="btn btn-danger" 
+                                    onClick={() => this.notificationHaveArgsUsingArrowFunction("Notification Have Args Using Arrow Function")}>Remove</div>
+                            <div className="btn btn-warning" 
+                                    onClick={this.notificationHaveArgsUsingBind.bind(this, "Notification Have Args Using Bind")}>Add</div>
+                            </div>
+                        </div>
                     </div>
                     </div>
                 </div>
