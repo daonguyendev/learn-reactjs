687cfe106ef512e03a04f8de09955369da06b446
Dao Nguyen <daonguyen.dev@gmail.com>
2018-06-11 16:35:41 +0700

    Using defaultValue to get props title between forms

--- 05_html_to_component/src/Component/Section/Section.js
+++ 05_html_to_component/src/Component/Section/Section.js
@@ -31,7 +31,9 @@ class Section extends Component {
         return (
             <div className="row">
                 <div className="form-group">
-                    <input type="text" name="name" className="form-control"/>   
+                    <input defaultValue={this.props.title} type="text" name="name" className="form-control"/>   
+                </div>
+                <div className="form-group">  
                     <div className="btn btn-success" onClick={() => this.clickSaveButton()}>Save</div>                       
                 </div>
             </div> 
