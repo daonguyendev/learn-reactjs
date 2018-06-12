9daa521379121ac71b4df9c1db536d26463daaae
daonguyen <daonguyen.dev@gmail.com>
2018-06-12 17:17:07 +0700

    React router, convert html to component

--- 07_react_router/src/components/App.js
+++ 07_react_router/src/components/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
+import './../css/App.css';
+import Nav from './Nav';
+import Home from './Home';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div>
+        <Nav/>
+        <Home/>
       </div>
     );
   }
