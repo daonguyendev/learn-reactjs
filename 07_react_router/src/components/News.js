6efbc6c8119e1224aa431870df06a857a8e567c8
daonguyen <daonguyen.dev@gmail.com>
2018-06-13 16:13:16 +0700

    Mapping data to component

--- 07_react_router/src/components/News.js
+++ 07_react_router/src/components/News.js
@@ -4,53 +4,56 @@ import Data from './Data.json';
 
 class News extends Component {
     render() {
-      console.log(Data);
+      var number = [1,3,5,7,9];
+      var number02 = number.map((value, key) => value*3 + " and position is: " + key);
+      console.log(number02);
+
         return (
             <div>
-        <header className="masthead">
-          <div className="container h-100">
-            <div className="row h-100">
-              <div className="col-lg-7 my-auto">
-                <div className="header-content mx-auto">
-                  <h1 className="mb-5">News</h1>
-                </div>
-              </div>
-              <div className="col-lg-5 my-auto">
-                <div className="device-container">
-                  <div className="device-mockup iphone6_plus portrait white">
-                    <div className="device">
-                      <div className="screen">
-                        {/* Demo image for screen mockup, you can put an image here, some HTML, an animation, video, or anything else! */}
-                        <img src="img/demo-screen-1.jpg" className="img-fluid" alt="For react router" />
+              <header className="masthead">
+                <div className="container h-100">
+                  <div className="row h-100">
+                    <div className="col-lg-7 my-auto">
+                      <div className="header-content mx-auto">
+                        <h1 className="mb-5">News</h1>
                       </div>
-                      <div className="button">
-                        {/* You can hook the "home button" to some JavaScript events or just remove it */}
+                    </div>
+                    <div className="col-lg-5 my-auto">
+                      <div className="device-container">
+                        <div className="device-mockup iphone6_plus portrait white">
+                          <div className="device">
+                            <div className="screen">
+                              {/* Demo image for screen mockup, you can put an image here, some HTML, an animation, video, or anything else! */}
+                              <img src="img/demo-screen-1.jpg" className="img-fluid" alt="For react router" />
+                            </div>
+                            <div className="button">
+                              {/* You can hook the "home button" to some JavaScript events or just remove it */}
+                            </div>
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
+              </header>
+              {/* begin news */}
+              <div className="container pt-3">
+                <div className="row nt-3">
+
+                  {
+                    Data.map((value, key) => {
+                      return (
+                        <NewItems key={key}
+                        image={value.image} 
+                        title={value.title}
+                        quote={value.quote}/>
+                      )
+                    })
+                  }
+
+                </div>
               </div>
             </div>
-          </div>
-        </header>
-        {/* begin news */}
-        <div className="container pt-3">
-          <div className="row nt-3">
-            <NewItems 
-              image="https://zmp3-photo.zadn.vn/banner/f/e/f/9/fef90bc4148b8765b3728ce809a23c89.jpg" 
-              title="Đừng Quên Tên Anh"
-              quote="Được biết, Hoa Vinh tên thật là Lê Tiến Anh"/>
-            <NewItems 
-              image="https://zmp3-photo.zadn.vn/banner/f/e/f/9/fef90bc4148b8765b3728ce809a23c89.jpg" 
-              title="Đừng Quên Tên Anh"
-              quote="Được biết, Hoa Vinh tên thật là Lê Tiến Anh"/>
-            <NewItems 
-              image="https://zmp3-photo.zadn.vn/banner/f/e/f/9/fef90bc4148b8765b3728ce809a23c89.jpg" 
-              title="Đừng Quên Tên Anh"
-              quote="Được biết, Hoa Vinh tên thật là Lê Tiến Anh"/>
-          </div>
-        </div>
-      </div>
         );
     }
 }
